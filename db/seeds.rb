83e1f543c3ee24b113b23ebb0290c41621f88a2b
Jeff George <webdevjeffus@gmail.com>
2016-01-05 18:44:52 -0500

    Create /db dir, /migrate dir, and seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+include Faker
+
+User.create( username: "george123", password: 12345678 )
+User.create( username: "thomas234", password: 23456789 )
+User.create( username: "john345", password: 34567890 )
